629e13648b742448c69363b9c12916bc7b7b01ac
slava hatnuke <slava.hatnuke@gmail.com>
2016-07-16 06:53:28 +0300

    wip

--- x.spec.js
+++ x.spec.js
@@ -4,54 +4,7 @@ var actives = require('./test/actives');
 
 describe('x.js', () => {
 
-    it('A', (done) => {
-        class Counter {
-            constructor(counter = 0) {
-                this.counter = counter;
-            }
-
-            get() {
-                return this.counter;
-            }
-
-            up() {
-                return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        this.counter++;
-                        reject();
-                    }, 10);
-                });
-            }
-        }
-
+    it('A', () => {
         let box = actives.Box.create();
-        box.add('counter', Counter);
-
-        let callCounter = 0;
-        box.connect('view', 'counter')
-            .state(({counter}) => {
-                callCounter++;
-                return {
-                    counter: counter.get()
-                };
-            });
-
-
-        expect(box.get('view/counter')).equal(0);
-        expect(callCounter).equal(1);
-
-
-        box.get('counter').up();
-
-        // async
-        expect(box.get('view/counter')).equal(0);
-        expect(callCounter).equal(2);
-
-
-        setTimeout(() => {
-            expect(box.get('view/counter')).equal(1);
-            expect(callCounter).equal(4);
-            done();
-        }, 20);
     });
 });
