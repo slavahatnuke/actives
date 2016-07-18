be77b5d4f03aa0cc7160af43f2ea4b6a2ba4b6f9
slava hatnuke <slava.hatnuke@gmail.com>
2016-07-18 20:54:37 +0300

    wip

--- src/Box/Definitions/Definitions.js
+++ src/Box/Definitions/Definitions.js
@@ -3,13 +3,13 @@ let Reflection = require('../../Reflection/Reflection');
 
 module.exports = class Definitions {
     constructor() {
-        this._definitions = new Map();
+        this.definitions = new Map();
         this.values = new Map();
     }
 
     add(name, definition) {
         if (definition instanceof Definition) {
-            this._definitions.set(definition.getName(), definition);
+            this.definitions.set(definition.getName(), definition);
         } else {
             this.values.set(name, definition);
         }
@@ -21,8 +21,8 @@ module.exports = class Definitions {
         }
 
         if(this.isDefinition(name))  {
-            this._definitions.get(name).reset();
-            this._definitions.delete(name);
+            this.definitions.get(name).reset();
+            this.definitions.delete(name);
         }
     }
 
@@ -32,7 +32,7 @@ module.exports = class Definitions {
 
     get(name) {
         if (this.isDefinition(name)) {
-            return this._definitions.get(name);
+            return this.definitions.get(name);
         } else {
             return this.values.get(name);
         }
@@ -47,7 +47,7 @@ module.exports = class Definitions {
     }
 
     isDefinition(name) {
-        return this._definitions.has(name);
+        return this.definitions.has(name);
     }
 
     isValue(name) {
@@ -64,14 +64,14 @@ module.exports = class Definitions {
 
     resolve(name, value) {
         if (this.isDefinition(name)) {
-            this._definitions.get(name).resolve(value)
+            this.definitions.get(name).resolve(value)
         } else {
             this.values.set(name, value);
         }
     }
 
     keys() {
-        return Reflection.iteratorToArray(this.values.keys()).concat(Reflection.iteratorToArray(this._definitions.keys()));
+        return Reflection.iteratorToArray(this.values.keys()).concat(Reflection.iteratorToArray(this.definitions.keys()));
     }
 
 
--- x.spec.js
+++ x.spec.js
@@ -3,18 +3,43 @@ var actives = require('./test/actives');
 
 describe('x.js', () => {
 
-    it('A', () => {
-        let app = actives.Box.create();
-        let box = actives.Box.create();
 
-        box.add('object', () => {
-            return {
-                counter: 1,
-                up: function () {
-                    this.counter++;
-                }
-            };
+    it('A', () => {
+        class Counter {
+            constructor(counter = 0) {
+                this.counter = counter;
+            }
+
+            get() {
+                return this.counter;
+            }
+
+            up() {
+                this.counter++;
+            }
+        }
+
+        let app = actives.Box.create();
+
+
+        let creator = () => {
+            let CounterBox = actives.Box.create();
+            CounterBox.add('defaultValue', 10);
+            CounterBox.add('counter', Counter, ['defaultValue']);
+
+            return CounterBox;
+        }
+
+        app.add('CounterBox1', creator());
+        expect(app.get('CounterBox1/counter/counter')).equal(10);
+
+        app.add('testValue', 25);
+        app.add('CounterBox2', creator(), {
+            defaultValue: () => 500,
+            valueA: 'testValue'
         });
 
+        expect(app.get('CounterBox2/counter/counter')).equal(500);
+        expect(app.get('CounterBox2/valueA')).equal(25);
     });
 });
